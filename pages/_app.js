4d87feaec240526c704238437e00e5750079e879
Binta F <45748934+bibine@users.noreply.github.com>
2021-04-01 00:46:32 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,50 +1,37 @@
-/* global process */
-import React, {useEffect, useState} from 'react';
+// pages/_app.js
+
+import App from 'next/app';
+import React from 'react';
 import '../style/scss/style.scss';
-import { useStore } from '../store';
-import { Provider  } from 'react-redux';
+import { wrapper } from '../store';
 import commerce from '../lib/commerce';
-import { loadStripe } from '@stripe/stripe-js';
-import { setCustomer } from '../store/actions/authenticateActions';
+import collections from '../lib/collections';
 
-const MyApp = ({Component, pageProps}) => {
-
-  const store = useStore(pageProps.initialState);
-  const [stripePromise, setStripePromise] = useState(null);
-
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) { // has API key
-      setStripePromise(loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY));
-    }
-
-    setCustomer();
-
-    commerce.products.list().then((res) => {
-      store.dispatch({
-        type: 'STORE_PRODUCTS',
-        payload: res.data
-      })
-    });
-
-    commerce.categories.list().then((res) => {
-      console.log(res.data)
-      store.dispatch({
-        type: 'STORE_CATEGORIES',
-        payload: res.data
-      })
-    });
-
-  }, [store])
-
-  return (
-    <Provider store={store}>
-      <Component
-        {...pageProps}
-        stripe={stripePromise}
-      />
-    </Provider>
-  );
-
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    // Fetch data on load
+    // Fetch categories
+    const categoriesResponse = await commerce.categories.list();
+    // Match static data record to API data to find category name
+    const categories = categoriesResponse.data.map(item => ({
+      ...collections.find(data => data.slug === item.slug),
+      ...item,
+    }));
+    // Fetch products
+    const { data: products } = await commerce.products.list();
+    // Allows store to be updated via the dispatch action
+    ctx.store.dispatch({ type: 'STORE_CATEGORIES', payload: categories });
+    ctx.store.dispatch({ type: 'STORE_PRODUCTS', payload: products });
+    return {
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+      },
+    };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
-
-export default MyApp;
+export default wrapper.withRedux(MyApp);
